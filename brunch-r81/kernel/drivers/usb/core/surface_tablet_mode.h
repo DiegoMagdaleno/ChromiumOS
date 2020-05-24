10e46df859a088c4f815fcb3518ed1a4664c9701
Diego Magdaleno <diegomagdaleno@protonmail.com>
2020-05-23 22:53:13 -0500

    Remove default brunch kernel

--- brunch-r81/kernel/drivers/usb/core/surface_tablet_mode.h
+++
@@ -1,3 +0,0 @@
-void surface_tablet_mode_notify(int bus, int port, bool connected);
-void surface_tablet_mode_init(struct usb_hub *hub, int bus);
-void surface_tablet_mode_fini(struct usb_hub *hub, int bus);
